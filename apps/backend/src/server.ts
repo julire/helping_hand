35b1a1e685dd400b4260e0a53dc9b5ed544989d9
Jordan Patel <logicaljordan247@gmail.com>
2022-12-28 13:12:00 +0100

    connected db

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -2,6 +2,17 @@ import { json, urlencoded } from "body-parser";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Successfully connected to the database");
+}).catch(err => {
+  console.log('Could not connect to the database. Error...', err);
+  process.exit();
+});
 
 export const createServer = () => {
   const app = express();
