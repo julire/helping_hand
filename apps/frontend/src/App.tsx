12ed7c7866f57d211ac5993dcbbe54e8dc983946
Jordan Patel <logicaljordan247@gmail.com>
2023-01-03 14:13:55 +0100

    removed templated content in App and removed default components from ui package

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,21 +1,13 @@
 import * as React from "react";
 import "./App.css";
-import { CounterButton, NewTabLink, NewButton } from "ui";
+import {  } from "ui";
 
 function App() {
   return (
     <div className="container">
       <h1 className="title">
-        Admin <br />
-        <span>Jordan's Kitchen Sink</span>
+        Helping Hand
       </h1>
-      <CounterButton />
-      <NewButton />
-      <p className="description">
-        Built With{" "}
-        <NewTabLink href="https://turbo.build/repo">Turborepo</NewTabLink> +{" "}
-        <NewTabLink href="https://vitejs.dev/">Vite</NewTabLink>
-      </p>
     </div>
   );
 }
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,4 +1,1 @@
 import * as React from "react";
-export { NewTabLink } from "./NewTabLink";
-export { CounterButton } from "./CounterButton";
-export { NewButton } from "./NewButton"
