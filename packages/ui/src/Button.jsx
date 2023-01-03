21f82e7171fa17d9401f360fd780f5df49c40941
Jordan Patel <logicaljordan247@gmail.com>
2023-01-03 16:00:57 +0100

    packages updated

--- packages/ui/src/Button.jsx
+++ packages/ui/src/Button.jsx
@@ -4,17 +4,7 @@ import * as React from "react";
 export const Button = () => {
   return (
     <div>
-      <button
-          style={{
-            background: "black",
-            color: "white",
-            border: "none",
-            padding: "0.5rem 1rem",
-            borderRadius: "0.25rem",
-            display: "inline-block",
-            cursor: "pointer",
-          }}
-        >
+      <button>
         </button>
       </div>
   );
