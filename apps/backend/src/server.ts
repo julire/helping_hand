fe6a529af5cbbd1cdcff90c1d789560dd042de82
amanpreetchalga <amanpreetchalga@gmail.com>
2023-01-05 17:00:27 +0100

    returning file to default

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -20,3 +20,12 @@ export const createServer = () => {
 
   return app;
 };
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://helpingHand:<password>@clusterhelpinghand.3kaeqwy.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
