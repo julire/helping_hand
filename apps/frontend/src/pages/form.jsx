1888167fab093b18639acf8a95942a2d84f7ad06
Gw3i <contact.johngoldco@gmail.com>
2023-01-05 20:35:13 +0100

    Add basic

--- apps/frontend/src/pages/form.jsx
+++ apps/frontend/src/pages/form.jsx
@@ -6,8 +6,21 @@ export function Form() {
       <h1>Create project</h1>
       <StyledForm>
         <label>
+          Title
           <input type="text" />
         </label>
+
+        <label>
+          Description
+          <input type="text" />
+        </label>
+
+        <label>
+          Image URL
+          <input type="url" />
+        </label>
+
+        <button type="submit">Submit</button>
       </StyledForm>
     </section>
   );
