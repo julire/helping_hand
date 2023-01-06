34038d95fab6b08aa0e29ac4c7fdb9c1d38c8b85
Jordan Patel <logicaljordan247@gmail.com>
2023-01-06 19:38:17 +0100

    refactor basic app page

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,32 +1,15 @@
-import * as React from "react";
-import "./App.css";
-import { Button } from "ui";
+import * as React from 'react';
+import './App.css';
+import { Button } from 'ui';
 
 function App() {
   return (
     <div className="container">
-      <h1 className="title">
-        Helping Hand
-      </h1>
+      <header>
+        <h1 className="title">Helping Hand</h1>
+      </header>
       <section className="card-list">
-        <div class="card-list--card">
-          <img src="" alt="" />
-          <div class="card--info">
-            <h2 className="card--title">Project 1 title</h2>
-            <p className="card--description">
-              This is a description of a project 1!
-            </p>
-          </div>
-        </div>
-        <div class="card-list--card">
-          <img src="" alt="" />
-          <div class="card--info">
-            <h2 className="card--title">Project 2 title</h2>
-            <p className="card--description">
-              This is a description of a project 2!
-            </p>
-          </div>
-        </div>
+        <div className="card"></div>
       </section>
     </div>
   );
