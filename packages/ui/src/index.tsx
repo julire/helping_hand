e564853436f35232decc95076cc88a535c16b075
LogicalJordan <92109628+Jordan-Patel@users.noreply.github.com>
2023-01-11 21:32:41 +0100

    Delete index.tsx

--- packages/ui/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export { StyledForm } from "./StyledForm";
-export { StyledInput } from "./StyledInput";
-export { StyledLabel } from "./StyledLabel";
-export { Button } from "./Button";
-export { CardWrapper, CardBody, CardHeader, CardHeading } from './StyledCard';
