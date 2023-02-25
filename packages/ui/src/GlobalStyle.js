b7cc56f582898ee0b2c52b597a555e97c38c232e
julire <jreshoeft@gmx.de>
2023-02-25 15:11:14 +0100

    Added color to form and Global Color

---
+++ packages/ui/src/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    :root {
+        --colors-highlight: rgb(227, 137, 51);
+        --colors-background: rgb(234, 234, 234)
+    }
+
+
+
+`;
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,20 +1,21 @@
-import * as React from 'react';
+import * as React from "react";
 export {
   StyledEmailIcon,
   StyledHomeIcon,
   StyledAddIcon,
   StyledBackIcon,
   StyledDeleteIcon,
-} from './StyledIcon';
-export { NewButton } from './NewButton';
-export { CardHeading } from './StyledCard';
-export { MainNav, NavLi, NavItem } from './StyledNavbar';
-export { StyledContainer } from './StyledContainer';
-export { StyledH1 } from './StyledH1';
-export { StyledH2 } from './StyledH2';
-export { StyledForm } from './StyledForm';
-export { StyledInput } from './StyledInput';
-export { StyledLabel } from './StyledLabel';
-export { StyledImg } from './StyledImg';
-export { StyledButton } from './StyledButton';
-export { StyledLink } from './StyledLink';
+} from "./StyledIcon";
+export { NewButton } from "./NewButton";
+export { CardHeading } from "./StyledCard";
+export { MainNav, NavLi, NavItem } from "./StyledNavbar";
+export { StyledContainer } from "./StyledContainer";
+export { StyledH1 } from "./StyledH1";
+export { StyledH2 } from "./StyledH2";
+export { StyledForm } from "./StyledForm";
+export { StyledInput } from "./StyledInput";
+export { StyledLabel } from "./StyledLabel";
+export { StyledImg } from "./StyledImg";
+export { StyledButton } from "./StyledButton";
+export { StyledLink } from "./StyledLink";
+export { GlobalStyles } from "./GlobalStyle";
